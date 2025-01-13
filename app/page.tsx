b01cdcdbb819bca4388bd0ad138e12c3e6ad5d8d
Sdunsmore2006 <sdunsmore244@gmail.com>
2025-01-13 15:00:47 -0500

    feat: refactors code

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import IntroSection from '../components/IntroSection'
-import ProjectsSection from '../components/ProjectsSection'
-import SkillsSection from '../components/SkillsSection'
-import ContactSection from '../components/ContactSection'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import IntroSection from './components/IntroSection'
+import ProjectsSection from './components/ProjectsSection'
+import SkillsSection from './components/SkillsSection'
+import ContactSection from './components/ContactSection'
 
 export default function Home() {
   return (
